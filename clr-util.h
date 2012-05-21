84472e47c179a7e6b5caac89b540c49b32dae7c7
Jeffrey Bush <jeff@coderforlife.com>
2012-05-21 11:10:23 -0700

    Added GPL license information.

--- clr-util.h
+++ clr-util.h
@@ -1,3 +1,19 @@
+// pe-file-editor: wrapper for many pe-file functions in a .NET library
+// Copyright (C) 2012  Jeffrey Bush  jeff@coderforlife.com
+//
+// This library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #pragma once
 
 // Types and Utilities for making C++/CLR more like C#
@@ -34,3 +50,6 @@ static array<T> ^to_managed(T *a, size_t l) { array<T> ^m = gcnew array<T>(l); S
 __forceinline const wchar_t *as_native(System::String ^s) { pin_ptr<const wchar_t> p = PtrToStringChars(s); return p; }
 __forceinline System::String ^as_managed(wchar_t *s) { return System::Runtime::InteropServices::Marshal::PtrToStringUni((System::IntPtr)s); }
 
+template<typename T> __forceinline T *ptr(T %a) { pin_ptr<T> p = &a; return p; }
+#define dwptr(a) (DWORD*)ptr(a)
+#define wptr(a) (WORD*)ptr(a)
